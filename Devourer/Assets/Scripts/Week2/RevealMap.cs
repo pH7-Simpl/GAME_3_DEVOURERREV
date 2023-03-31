1c686d2dd090d547cd4c6b2c5839634803c84d7f
Ba3yoo <115632689+Ba3yoo@users.noreply.github.com>
2023-04-01 00:05:05 +0700

    Update RevealMap.cs

--- Devourer/Assets/Scripts/Week2/RevealMap.cs
+++ Devourer/Assets/Scripts/Week2/RevealMap.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class RevealMap : MonoBehaviour
 {
-    [SerializeField] private Transform positionCheck;
     [SerializeField] private LayerMask playerLayer;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
