2de2a913094b1ef56ca0510ef4ddd49a0d689079
pH7-Simpl <eleazar.tadeo29@gmail.com>
2023-04-02 00:45:57 +0700

    Enemy cant go through ground

--- Devourer/Assets/Scripts/Week2/EnemyAI.cs
+++ Devourer/Assets/Scripts/Week2/EnemyAI.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
 
@@ -19,22 +18,28 @@ public class EnemyAI : MonoBehaviour
     private float dis;
     private float maxEnemyHealth;
     private float enemyHealth;
-    public float GetEnemyHealth() {
+    public float GetEnemyHealth()
+    {
         return enemyHealth;
     }
     private bool showHB;
     private bool hit;
-    public bool IsHit() {
-        return hit;
-    }
-    // Start is called before the first frame update
-    private void OnTriggerEnter2D(Collider2D other) {
+    private bool groundHit;
+    private GameObject healthBar;
+    private float showHBCooldown = 0f;
+    private bool hasCollided;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (hasCollided)
+        {
+            return;
+        }
         if (other.gameObject.name == "windSlash")
         {
+            Destroy(other.gameObject);
             enemyHealth -= 25;
             showHB = true;
             StartCoroutine(HitEffect(0.5f));
-            Destroy(other.gameObject);
         }
         if (other.gameObject.name == "lightningDash")
         {
@@ -43,9 +48,37 @@ public class EnemyAI : MonoBehaviour
             StartCoroutine(HitEffect(0.5f));
             Destroy(other.gameObject);
         }
-        if(other.gameObject.tag == "Player") {
+        if (other.gameObject.tag == "Player")
+        {
             Debug.Log("Test");
         }
+        StartCoroutine(ColliderRefresher());
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name == "fireBreath")
+        {
+            enemyHealth -= 1;
+            showHB = true;
+            StartCoroutine(HitEffect(0.3f));
+        }
+        if (other.gameObject.layer == 7)
+        {
+            StartCoroutine(HitEffect(0.1f));
+        }
+        if(other.gameObject.layer == 6) {
+            if(transform.position.y >= other.transform.position.y) {
+                rb.velocity = new Vector2(rb.velocity.x, 1f);
+            } else {
+                rb.velocity = new Vector2(rb.velocity.x, -1f);
+            }
+        }
+    }
+    private IEnumerator ColliderRefresher()
+    {
+        hasCollided = true;
+        yield return new WaitForSeconds(Time.deltaTime);
+        hasCollided = false;
     }
     void Start()
     {
@@ -59,9 +92,13 @@ public class EnemyAI : MonoBehaviour
         enemyGFX = transform.Find("FlyEnemyGFX");
         enemyHealth = 100f;
         InvokeRepeating("UpdatePath", 0f, 0.5f);
+        hit = false;
+        groundHit = false;
         showHB = false;
         maxEnemyHealth = 100f;
         enemyHealth = maxEnemyHealth;
+        healthBar = transform.GetChild(1).gameObject;
+        hasCollided = false;
     }
     private void UpdatePath()
     {
@@ -80,8 +117,20 @@ public class EnemyAI : MonoBehaviour
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        if (enemyHealth <= 0)
+        {
+            enemyHealth = 0;
+        }
+    }
     void FixedUpdate()
     {
+        if (enemyHealth <= 0)
+        {
+            StartCoroutine("Die");
+        }
+        ShowHealthBar();
         if (path == null)
         {
             return;
@@ -99,7 +148,21 @@ public class EnemyAI : MonoBehaviour
         dir = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
         force = dir * speed * Time.deltaTime;
 
-        rb.AddForce(force);
+        if (!hit)
+        {
+            rb.AddForce(force);
+        }
+        else
+        {
+            if (force.x >= 0.01)
+            {
+                rb.velocity = new Vector2(-3f, 0f);
+            }
+            else if (force.x <= -0.01)
+            {
+                rb.velocity = new Vector2(3f, 0f);
+            }
+        }
 
         dis = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
 
@@ -116,7 +179,8 @@ public class EnemyAI : MonoBehaviour
         {
             enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
         }
-        if(reachEndOfPath) {
+        if (reachEndOfPath)
+        {
             Debug.Log("test");
         }
     }
@@ -126,4 +190,29 @@ public class EnemyAI : MonoBehaviour
         yield return new WaitForSeconds(duration);
         hit = false;
     }
+    private IEnumerator Die()
+    {
+        //die animation
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
+    private void ShowHealthBar()
+    {
+        if (showHB)
+        {
+            showHBCooldown = 1f;
+            healthBar.SetActive(true);
+            showHB = false;
+        }
+        if (showHBCooldown >= 0f)
+        {
+            showHBCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            healthBar.SetActive(false);
+        }
+    }
+    
 }
