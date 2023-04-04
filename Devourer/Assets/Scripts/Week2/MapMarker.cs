d5ef6d3c0bf377c2eaefc2f5970a83360f47babb
pH7-Simpl <eleazar.tadeo29@gmail.com>
2023-04-04 23:55:44 +0700

    agadg

--- Devourer/Assets/Scripts/Week2/MapMarker.cs
+++ Devourer/Assets/Scripts/Week2/MapMarker.cs
@@ -9,21 +9,20 @@ public class MapMarker : MonoBehaviour
     public Camera MapCamera;
     private gameManager gm;
     private void Awake()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-        MC = Camera.main;
-        MapCamera = GameObject.Find("Map Camera").GetComponent<Camera>();
-        MapCamera.transform.SetParent(transform);
-        MapCamera.enabled = false;
-        MC.enabled = true;
-        gm = FindObjectOfType<gameManager>();
-    }
+{
+    player = GameObject.FindGameObjectWithTag("Player");
+    MC = Camera.main;
+    MapCamera = GameObject.Find("Map Camera").GetComponent<Camera>();
+    MapCamera.transform.SetParent(transform);
+    // disable the MapCamera after it has been assigned to the MapCamera variable
+    MapCamera.enabled = false; 
+    MC.enabled = true;
+    gm = FindObjectOfType<gameManager>();
+}
 
     private void Update()
     {
-        if(player != null) {
-            transform.position = player.transform.position;
-        }
+        transform.position = player.transform.position;
         MapCamera.transform.position = player.transform.position + new Vector3(0, 0, -5f);
         MapCamera.enabled = gm.IsSeeMap();
         MC.enabled = !gm.IsSeeMap();
