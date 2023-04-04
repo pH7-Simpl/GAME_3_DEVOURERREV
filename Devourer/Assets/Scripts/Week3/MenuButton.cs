019878f0ea425a980779d734ccb8d9129f56302c
pH7-Simpl <eleazar.tadeo29@gmail.com>
2023-04-04 20:06:12 +0700

    a

--- Devourer/Assets/Scripts/Week3/MenuButton.cs
+++ Devourer/Assets/Scripts/Week3/MenuButton.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour
 {
+    private Animator animator1;
+    private Animator animator2;
+    private void Awake() {
+        animator1 = transform.GetChild(0).GetChild(1).GetComponent<Animator>();
+        animator2 = transform.GetChild(1).GetChild(1).GetComponent<Animator>();
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(0);
@@ -13,5 +19,11 @@ public class MenuButton : MonoBehaviour
     {
         Debug.Log("I Quit");
         Application.Quit();
+    }
+    public void SetHover(bool x) {
+        animator1.SetBool("hover", true);
+    }
+    public void SetExit(bool x) {
+        animator1.SetBool("hover", false);
     }
 }
