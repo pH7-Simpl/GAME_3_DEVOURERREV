5607077d80cc721cc62a1f14aab849c7f9d39f5f
pH7-Simpl <eleazar.tadeo29@gmail.com>
2023-04-04 14:11:16 +0700

    aeaf
    geafesd

--- Devourer/Assets/Scripts/Week3/BossAttacks.cs
+++ Devourer/Assets/Scripts/Week3/BossAttacks.cs
@@ -4,25 +4,54 @@ using UnityEngine;
 
 public class BossAttacks : MonoBehaviour
 {
+    [SerializeField] private GameObject lightningStrike;
+    [SerializeField] private GameObject waterStrike;
+    [SerializeField] private GameObject windStrike;
+    [SerializeField] private GameObject fireStrike;
     // Start is called before the first frame update
     private float duration = 0f;
-    Random rdm = new Random();
+    private bool attacking = false;
 
     // Update is called once per frame
     void Update()
     {
-        AtkStart(rdm.Next(4));
+        if(!attacking) {
+            StartCoroutine(AttackSequence());
+            attacking = true;
+        }
     }
-    private void AtkStart(int random){
-        switch (random){
+
+    private IEnumerator AttackSequence() {
+        AtkStart(Random.Range(0, 4));
+        yield return new WaitForSeconds(4f);
+        attacking = false;
+    }
+    private void AtkStart(int choice){
+        switch (choice){
             case 0:
+                StartCoroutine(LightningStrike());
             break;
             case 1:
+                StartCoroutine(WaterStrike());
             break;
             case 2:
+                StartCoroutine(WindStrike());
             break;
             case 3:
+                StartCoroutine(FireStrike());
             break;
         }
+    }
+    private IEnumerator LightningStrike() {
+        yield return new WaitForSeconds(1f);
+    }
+    private IEnumerator WaterStrike() {
+        yield return new WaitForSeconds(1f);
+    }
+    private IEnumerator WindStrike() {
+        yield return new WaitForSeconds(1f);
+    }
+    private IEnumerator FireStrike() {
+        yield return new WaitForSeconds(1f);
     }
 }
