9114f5536dd34b3a2d64c9c116b50a023a0a889b
pH7-Simpl <eleazar.tadeo29@gmail.com>
2023-04-07 18:44:53 +0700

    fullscreen build and bugfix for credits

--- Devourer/Assets/Scripts/Week3/StartGame.cs
+++ Devourer/Assets/Scripts/Week3/StartGame.cs
@@ -1,10 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StartGame : GoingOutside
 {
     void Awake() {
+        Screen.fullScreen = true;
         StartScene();
     }
 }
